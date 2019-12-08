f0b271ecbc7d778fc4bfa042f4fe2ce4318fa2c3
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2019-12-08 17:31:01 +0900

    Error .. 알수없음

--- src/java_term_project/Drink.java
+++ src/java_term_project/Drink.java
@@ -4,22 +4,23 @@ package java_term_project;
 public class Drink {
 	private int number;
 
-	//생성자로 음료의 갯수 초기화 함
+	// 생성자로 음료의 갯수 초기화 함
 	public Drink(int number) {
 		this.number = number;
 	}
-	
+
 	public int getNumber() {
 		return number;
 	}
+
 	public void setNumber(int number) {
 		this.number = number;
 	}
-	
+
 	public void MinusNumber(int number, int minus) {
 		this.number = number - minus;
 	}
-	
+
 	public void setNumber(int number, int how) {
 		this.number = number - how;
 	}
@@ -27,101 +28,91 @@ public class Drink {
 }
 
 // 물
-class Water_class extends Drink{
+class Water_class extends Drink {
 	private int water;
 
 	public Water_class(int number, int water) {
 		super(number);
 		this.water = water;
 	}
-	
 
 	public int getWater() {
 		return water;
 	}
+
 	public void setWater(int water) {
 		this.water = water;
 	}
 
-
 // 커피
-class Coffee_class extends Drink{
-	private int coffee;
+class Coffee_class extends Drink {
+		private int coffee;
 
-	public Coffee_class(int number, int coffee) {
-		super(number);
-		this.coffee = coffee;
-	}
-	
-	public int getCoffee() {
-		return coffee;
-	}
-	public void setCoffee(int coffee) {
-		this.coffee = coffee;
-	}
-}
+		public Coffee_class(int number, int coffee) {
+			super(number);
+			this.coffee = coffee;
+		}
 
-//고급커피
-class High_Quality_Coffee_class extends Drink {
-	public int high_quality_coffee;
-	public High_Quality_Coffee_class(int number,int High_Coffee) {
-		super(number);
-		this.high_quality_coffee = High_Coffee;
-	}
-}
+		public int getCoffee() {
+			return coffee;
+		}
 
+		public void setCoffee(int coffee) {
+			this.coffee = coffee;
+		}
+	}
 
 // 이온음료
-class  Ion_Beverage_class extends Drink{
-	private int ion_beverage;
+	class Ion_Beverage_class extends Drink {
+		private int ion_beverage;
 
-	public Ion_Beverage_class(int number, int ion_beverage) {
-		super(number);
-		this.ion_beverage = ion_beverage;
-	}
-	
-	public int getIon_beverage() {
-		return ion_beverage;
-	}
-	public void setIon_beverage(int ion_beverage) {
-		this.ion_beverage = ion_beverage;
-	}
-}
+		public Ion_Beverage_class(int number, int ion_beverage) {
+			super(number);
+			this.ion_beverage = ion_beverage;
+		}
 
+		public int getIon_beverage() {
+			return ion_beverage;
+		}
+
+		public void setIon_beverage(int ion_beverage) {
+			this.ion_beverage = ion_beverage;
+		}
+	}
 
 // 고급커피
-class High_Quality_Coffee_class extends Drink {
-	private int high_quality_coffee;
+	class High_Quality_Coffee_class extends Drink {
+		private int high_quality_coffee;
 
-	public High_Quality_Coffee_class(int number,int High_Coffee) {
-		super(number);
-		this.high_quality_coffee = High_Coffee;
-	}
-	
-	public int getHigh_quality_coffee() {
-		return high_quality_coffee;
-	}
-	public void setHigh_quality_coffee(int high_quality_coffee) {
-		this.high_quality_coffee = high_quality_coffee;
-	}
-}
+		public High_Quality_Coffee_class(int number, int High_Coffee) {
+			super(number);
+			this.high_quality_coffee = High_Coffee;
+		}
 
+		public int getHigh_quality_coffee() {
+			return high_quality_coffee;
+		}
+
+		public void setHigh_quality_coffee(int high_quality_coffee) {
+			this.high_quality_coffee = high_quality_coffee;
+		}
+	}
 
 // 탄산음료
-class Soda_class extends Drink {
-	private int soda;
+	class Soda_class extends Drink {
+		private int soda;
 
-	public Soda_class(int number, int soda) {
-		super(number);
-		this.soda = soda;
-	}
-	
-	public int getSoda() {
-		return soda;
-	}
-	public void setSoda(int soda) {
-		this.soda = soda;
-	}
+		public Soda_class(int number, int soda) {
+			super(number);
+			this.soda = soda;
+		}
 
+		public int getSoda() {
+			return soda;
+		}
+
+		public void setSoda(int soda) {
+			this.soda = soda;
+		}
+	}
 }
-
--- src/java_term_project/GUI.java
+++ src/java_term_project/GUI.java
@@ -47,13 +47,6 @@ public class GUI extends JFrame {
 		 * Class init Area
 		 */
 
-		// Drink 초기화
-		Water_class water = new Water_class(3, 450);
-		Coffee_class coffee = new Coffee_class(3, 500);
-		Ion_Beverage_class ion = new Ion_Beverage_class(3, 550);
-		High_Quality_Coffee_class high_coffee = new High_Quality_Coffee_class(3, 700);
-		Soda_class soda = new Soda_class(3, 750);
-
 		// Money 초기화
 		Money10 money10 = new Money10(10, 5);
 		Money50 money50 = new Money50(50, 5);
@@ -61,6 +54,8 @@ public class GUI extends JFrame {
 		Money500 money500 = new Money500(500, 5);
 		Money1000 money1000 = new Money1000(1000, 5);
 		
+		
+		// Drink 초기화
 		Water_class water = new Water_class(3,450);
 		Coffee_class coffee = new Coffee_class(3,500);
 		Ion_Beverage_class ion = new Ion_Beverage_class (3,550);
